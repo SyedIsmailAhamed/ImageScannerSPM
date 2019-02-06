8ac1279ab60dfc7289939a77f7017fb5db5a64c7
Syed Ismail Ahamed <syedismailahamed@Arslan-Asims-iMac.local>
2019-02-06 12:02:25 +0400

    initial commit

---
+++ OCR/OCRImageScannerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OCRImageScannerViewController.swift
+//  OCR
+//
+//  Created by Syed Ismail Ahamed on 2/6/19.
+//  Copyright © 2019 Syed Ismail Ahamed. All rights reserved.
+//
+
+import UIKit
+
+class OCRImageScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OCRImageScanner/OCRImageScanner.h
@@ -0,0 +1,19 @@
+//
+//  OCRImageScanner.h
+//  OCRImageScanner
+//
+//  Created by Syed Ismail Ahamed on 2/6/19.
+//  Copyright © 2019 Syed Ismail Ahamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCRImageScanner.
+FOUNDATION_EXPORT double OCRImageScannerVersionNumber;
+
+//! Project version string for OCRImageScanner.
+FOUNDATION_EXPORT const unsigned char OCRImageScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCRImageScanner/PublicHeader.h>
+
+
